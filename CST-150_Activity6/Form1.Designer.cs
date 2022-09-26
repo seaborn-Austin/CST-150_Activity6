eaa1a59b70a53956b7d6b65d4fcc80ee5dc54c36
seaborn-Austin <64162552+seaborn-Austin@users.noreply.github.com>
2022-09-26 05:35:44 -0500

    Add project files.

---
+++ CST-150_Activity6/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CST_150_Activity6
+{
+    partial class wtFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wtLbLbl = new System.Windows.Forms.Label();
+            this.wtLbTxt = new System.Windows.Forms.TextBox();
+            this.wtKgLbl = new System.Windows.Forms.Label();
+            this.wtKgTxt = new System.Windows.Forms.TextBox();
+            this.wtBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wtLbLbl
+            // 
+            this.wtLbLbl.AutoSize = true;
+            this.wtLbLbl.Location = new System.Drawing.Point(12, 19);
+            this.wtLbLbl.Name = "wtLbLbl";
+            this.wtLbLbl.Size = new System.Drawing.Size(178, 13);
+            this.wtLbLbl.TabIndex = 0;
+            this.wtLbLbl.Text = "Please enter your weight in pounds: ";
+            // 
+            // wtLbTxt
+            // 
+            this.wtLbTxt.Location = new System.Drawing.Point(196, 12);
+            this.wtLbTxt.Name = "wtLbTxt";
+            this.wtLbTxt.Size = new System.Drawing.Size(100, 20);
+            this.wtLbTxt.TabIndex = 1;
+            // 
+            // wtKgLbl
+            // 
+            this.wtKgLbl.AutoSize = true;
+            this.wtKgLbl.Location = new System.Drawing.Point(12, 49);
+            this.wtKgLbl.Name = "wtKgLbl";
+            this.wtKgLbl.Size = new System.Drawing.Size(125, 13);
+            this.wtKgLbl.TabIndex = 2;
+            this.wtKgLbl.Text = "Your weight in Kilograms:";
+            // 
+            // wtKgTxt
+            // 
+            this.wtKgTxt.Location = new System.Drawing.Point(143, 42);
+            this.wtKgTxt.Name = "wtKgTxt";
+            this.wtKgTxt.Size = new System.Drawing.Size(100, 20);
+            this.wtKgTxt.TabIndex = 3;
+            // 
+            // wtBtn
+            // 
+            this.wtBtn.Location = new System.Drawing.Point(65, 100);
+            this.wtBtn.Name = "wtBtn";
+            this.wtBtn.Size = new System.Drawing.Size(75, 23);
+            this.wtBtn.TabIndex = 4;
+            this.wtBtn.Text = "Result";
+            this.wtBtn.UseVisualStyleBackColor = true;
+            this.wtBtn.Click += new System.EventHandler(this.wtBtn_Click);
+            // 
+            // wtFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 300);
+            this.Controls.Add(this.wtBtn);
+            this.Controls.Add(this.wtKgTxt);
+            this.Controls.Add(this.wtKgLbl);
+            this.Controls.Add(this.wtLbTxt);
+            this.Controls.Add(this.wtLbLbl);
+            this.Name = "wtFrm";
+            this.Text = "Weight Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label wtLbLbl;
+        private System.Windows.Forms.TextBox wtLbTxt;
+        private System.Windows.Forms.Label wtKgLbl;
+        private System.Windows.Forms.TextBox wtKgTxt;
+        private System.Windows.Forms.Button wtBtn;
+    }
+}
+
---
+++ CST-150_Activity6/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150_Activity6
+{
+    public partial class wtFrm : Form
+    {
+        public wtFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void wtBtn_Click(object sender, EventArgs e)
+        {
+            double wtPounds = double.Parse(wtLbTxt.Text);
+            double wtKg = wtPounds * 0.45359237;
+            double result = Math.Round((Double)wtKg, 3);
+            wtKgTxt.Text = result.ToString();
+        }
+    }
+}
